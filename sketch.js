e8a1bd28ce877600884baef25ac1fbd195b6e7ff
Anuj Doddakaragi <83464826+nimbus2009@users.noreply.github.com>
2021-06-16 21:34:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var balls=[];
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+}
+
+function draw() {
+  background(0,191,255);
+  for(var x=0;x<balls.length;x++) {
+    balls[x].update();
+    balls[x].display();
+  }
+
+  text("Click anywhere on the \nscreen to create bubbles!",20,20); 
+  text("Press any key on the \nkeyboard to destroy bubbles!",20,70);
+
+  if(touches.length>0) {
+    var b=new Ball(touches[0],touches[1],20);
+    balls.push(b);
+
+    touches=[];
+  }
+
+  console.log(touches[0]);
+
+}
+
+function keyPressed() {
+  balls.pop();
+}
+
+function mousePressed() {
+  var b=new Ball(mouseX,mouseY,20);
+  balls.push(b);
+}
