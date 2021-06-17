d4e8ea7134a12da28592ac2731949fc9339cb79b
Anuj Doddakaragi <83464826+nimbus2009@users.noreply.github.com>
2021-06-17 11:26:01 +0530

    Updated sketch.js

--- sketch.js
+++ sketch.js
@@ -19,13 +19,13 @@ function draw() {
   text("Click anywhere on the \nscreen to create bubbles!",20,20); 
   text("Press any key on the \nkeyboard to destroy bubbles!",20,70);
 
-  if(touches.length>0) {
+  if(touches.length>0&&(touches[0]>des.position.x+100)) {
     var b=new Ball(touches[0],touches[1],20);
     balls.push(b);
 
     touches=[];
   }
-  if(touches.length>0&&(des.position.x-50<touches[0]>des.position.x+50)&&(des.position.y-50<touches[0]>des.position.y+50)) {
+  if(touches.length>0&&(des.position.x-50<touches[0]<des.position.x+50)&&(des.position.y-50<touches[0]<des.position.y+50)) {
     var b=new Ball(touches[0],touches[1],20);
     balls.push(b);
 
